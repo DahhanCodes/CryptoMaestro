f8aba20e79d1e119c81a64e8c6191f0cd8930b71
Dirie <Esse.Dirie@moneris.com>
2021-12-23 17:26:17 -0500

    testing search bar

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
-// var symbol = "BTC"
+const form = document.querySelector('#searchForm')
+form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const searchTerm = form.elements.query.value;
+    const res = await axios.get(`https://www.alphavantage.co/query?function=CRYPTO_INTRADAY&symbol=${searchTerm}&market=USD&interval=5min&apikey=98J29VO1JNI4HWKO`);
+    console.log(res.data);
+})
 
-var urlETH = "https://www.alphavantage.co/query?function=CRYPTO_INTRADAY&symbol=ETH&market=USD&interval=5min&apikey=98J29VO1JNI4HWKO";
-var urlBTC = "https://www.alphavantage.co/query?function=CRYPTO_INTRADAY&symbol=BTC&market=USD&interval=5min&apikey=98J29VO1JNI4HWKO";
-var urlCardano ="https://www.alphavantage.co/query?function=CRYPTO_INTRADAY&symbol=ADA&market=USD&interval=5min&apikey=98J29VO1JNI4HWKO";
+// // ETHER //
+// fetch(urlETH)
+//   .then(function (data) {
+//     return data.json();
+//   })
+//   .then(function (res) {
+//     console.log(res);
+//   });
 
-// ETHER //
-fetch(urlETH)
-  .then(function (data) {
-    return data.json();
-  })
-  .then(function (res) {
-    console.log(res);
-  });
+// // BITCOIN//
+// fetch(urlBTC)
+//   .then(function (data) {
+//     return data.json();
+//   })
+//   .then(function (res) {
+//     console.log(res);
 
-// BITCOIN//
-fetch(urlBTC)
-  .then(function (data) {
-    return data.json();
-  })
-  .then(function (res) {
-    console.log(res);
+//   });
 
-  });
-
-// CARDANO //
-fetch(urlCardano)
-  .then(function (data) {
-    return data.json();
-  })
-  .then(function (res) {
-    console.log(res);
-  });
+// // CARDANO //
+// fetch(urlCardano)
+//   .then(function (data) {
+//     return data.json();
+//   })
+//   .then(function (res) {
+//     console.log(res);
+//   });
 
 
-  
+
 // <!-- API KEY -->
 //  98J29VO1JNI4HWKO
