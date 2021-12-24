d0883913139e4475d4a590fec42b3793244c98a9
Dirie <Esse.Dirie@moneris.com>
2021-12-23 19:50:45 -0500

    fixed api

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 const form = document.querySelector('#searchForm')
+
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
     const searchTerm = form.elements.query.value;
     const res = await axios.get(`https://www.alphavantage.co/query?function=CRYPTO_INTRADAY&symbol=${searchTerm}&market=USD&interval=5min&apikey=98J29VO1JNI4HWKO`);
     console.log(res.data);
+    console.log(res.data['Meta Data']);
+    console.log(res.data['Meta Data']['7. Interval']);
+
+    
+    
+// Object.values(res.data['Meta Data']).forEach(element => {
+//     console.log(element)
+// });
+    
+    
+    
+    
+    
+    
+    
+    // var info = [res.data]
+    // console.log(info)
 })
+
+
+
 
 // // ETHER //
 // fetch(urlETH)
