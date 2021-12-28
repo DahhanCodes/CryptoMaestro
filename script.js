a2c2de88e8e5249b3e46e38db429867eef603080
edirie <essedirie@gmail.com>
2021-12-27 19:11:38 -0500

    more changes to api

--- script.js
+++ script.js
@@ -3,12 +3,23 @@ const form = document.querySelector('#searchForm')
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
     const searchTerm = form.elements.query.value;
-    const res = await axios.get(`https://www.alphavantage.co/query?function=CRYPTO_INTRADAY&symbol=${searchTerm}&market=USD&interval=5min&apikey=98J29VO1JNI4HWKO`);
+    const res = await axios.get(`https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=${searchTerm}&market=USD&interval=5min&apikey=98J29VO1JNI4HWKO`);
     console.log(res.data);
     console.log(res.data['Meta Data']);
-    console.log(res.data['Meta Data']['7. Interval']);
+   
+  
+// Variables for Crypto Search Criteria
 
-    
+    var coinSearched = (res.data['Meta Data']['3. Digital Currency Name'])
+    var dailyHigh = (res.data['Time Series (Digital Currency Daily)']["2021-12-27"]["2a. high (USD)"])
+    var dailyClose = (res.data['Time Series (Digital Currency Daily)']["2021-12-27"]["4a. close (USD)"])
+    var marketCap = (res.data['Time Series (Digital Currency Daily)']["2021-12-27"]["6. market cap (USD)"])
+
+    document.getElementById("coinname").innerHTML = coinSearched;
+    document.getElementById("dailyhigh").innerHTML = dailyHigh;
+    document.getElementById("closeprice").innerHTML = dailyClose;
+    document.getElementById("marketcap").innerHTML = marketCap;
+
     
 // Object.values(res.data['Meta Data']).forEach(element => {
 //     console.log(element)
